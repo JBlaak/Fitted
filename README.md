488273cfe0dbe632d89c972a84afee3936c7d692
Joris Blaak <joris@label305.com>
2016-05-28 22:23:14 +0200

    Allowing arguments to be merged with url

--- README.md
+++ README.md
@@ -5,16 +5,25 @@ Fitted
 
 ES7 decorator to do http request, currently in experimental fase! But look how awesome this is:
 
-```
+```javascript
 import {get} from 'fitted';
 
 class HackerNews {
 
     @get('https://hacker-news.firebaseio.com/v0/topstories.json')
     topstories (request, response) {
-      return request(response);
+      return request({}, response);
     }
-
+    
+    @get('https://hacker-news.firebaseio.com/v0/item/{id}.json')
+    item (id, request, response) {
+      return request(
+        {
+          id: id
+        },
+        response
+      );
+    }
 }
 ```
 
@@ -23,5 +32,6 @@ And fetch:
 ```
 const hackerNews = new HackerNews();
 const topstories = await hackerNews.topstories();
+const item = await hackerNews.item(9786706);
 ```
 
--- tests/http.test.js
+++ tests/http.test.js
@@ -11,7 +11,7 @@ describe('HttpDecorator', function () {
 
         @get('https://hacker-news.firebaseio.com/v0/topstories.json')
         topstories (request, response) {
-          return request(response);
+          return request({}, response);
         }
 
       }
@@ -23,6 +23,31 @@ describe('HttpDecorator', function () {
       /* Then */
       expect(topstories).to.be.an(Array);
       expect(topstories.length).to.be.greaterThan(0);
+    });
+
+    it('should be able to fetch single story', async function () {
+      /* Given */
+      class HackerNews {
+
+        @get('https://hacker-news.firebaseio.com/v0/item/{id}.json')
+        item (id, request, response) {
+          return request(
+            {
+              id: id
+            },
+            response
+          );
+        }
+
+      }
+
+      /* When */
+      var hackerNews = new HackerNews();
+      const item = await hackerNews.item(2921983);
+
+      /* Then */
+      expect(item).to.be.ok();
+      expect(item.by).to.be("norvig");
     });
 
   });
