8e039a54c05a5312fd205493ddae8c70e3f18775
Joris Blaak <joris@label305.com>
2016-06-01 11:24:06 +0200

    Optimized tests, no actual requests to HackerNews anymore

--- src/request.js
+++ src/request.js
@@ -1,11 +1,11 @@
 import UrlTemplate from 'url-template';
-import HttpFactory from './http';
+import Driver from './driver';
 import ProcessorFactory from './processor_factory';
 
 export default function request (url, config, res) {
   return new Promise((resolve, reject) => {
     const expandedUrl = UrlTemplate.parse(url).expand(config.template || {});
-    HttpFactory(expandedUrl, config, (response) => {
+    Driver(expandedUrl, config, (response) => {
 
       if (typeof res.processor != 'function') {
         res.processor = ProcessorFactory(response);
---
+++ tests/data/response.test.js
@@ -0,0 +1,44 @@
+import expect from 'expect.js';
+import Response from '../../src/data/response';
+
+describe('Response', function () {
+
+  describe('isOk', function () {
+    it('should be false when an error is set', function () {
+      /* Given */
+      var response = new Response();
+      response.setStatus(200);
+      response.setError(new Error('Something went booboo'));
+
+      /* When */
+      var ok = response.isOk();
+
+      /* Then */
+      expect(ok).to.be(false);
+    });
+
+    it('should be true when a 2xx status code is passed', function () {
+      /* Given */
+      var response = new Response();
+      response.setStatus(201);
+
+      /* When */
+      var ok = response.isOk();
+
+      /* Then */
+      expect(ok).to.be(true);
+    });
+
+    it('should be false when a non 2xx status code is passed', function () {
+      /* Given */
+      var response = new Response();
+      response.setStatus(400);
+
+      /* When */
+      var ok = response.isOk();
+
+      /* Then */
+      expect(ok).to.be(false);
+    })
+  })
+});
