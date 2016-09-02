5cb87b70ee09fe7d7042be41bf6f858cfd4e7980
Joris Blaak <joris@label305.com>
2016-09-02 11:08:03 +0200

    Added passing headers to Superagent driver [fixes #15]

--- src/drivers/superagent.js
+++ src/drivers/superagent.js
@@ -1,68 +1,86 @@
-import Request from 'superagent';
-import Response from '../data/response';
+import Superagent from "superagent";
+import Response from "../data/response";
 
-function get (url, config, callback) {
-  let req = Request.get(url);
-  if (config.query) {
-    req = req.query(config.query);
-  }
-  req.end(end(callback));
+function get(url, config, callback) {
+    let req = Superagent.get(url);
+    if (config.query) {
+        req = req.query(config.query);
+    }
+    if (config.headers) {
+        req = passHeaders(config.headers);
+    }
+    req.end(end(callback));
 }
 
-function post (url, config, callback) {
-  let req = Request.post(url);
-  if (config.data) {
-    req = req.send(config.data);
-  }
-  req.end(end(callback));
+function post(url, config, callback) {
+    let req = Superagent.post(url);
+    if (config.data) {
+        req = req.send(config.data);
+    }
+    if (config.headers) {
+        req = passHeaders(config.headers);
+    }
+    req.end(end(callback));
 }
 
-function put (url, config, callback) {
-  let req = Request.post(url);
-  if (config.data) {
-    req = req.send(config.data);
-  }
-  req.end(end(callback));
+function put(url, config, callback) {
+    let req = Superagent.post(url);
+    if (config.data) {
+        req = req.send(config.data);
+    }
+    if (config.headers) {
+        req = passHeaders(config.headers);
+    }
+    req.end(end(callback));
 }
 
-function destroy (url, config, callback) {
-  let req = Request.del(url);
-  if (config.data) {
-    req = req.send(config.data);
-  }
-  req.end(end(callback));
+function destroy(url, config, callback) {
+    let req = Superagent.del(url);
+    if (config.data) {
+        req = req.send(config.data);
+    }
+    if (config.headers) {
+        req = passHeaders(config.headers);
+    }
+    req.end(end(callback));
 }
 
 export default (url, config, callback) => {
-  switch (config.method) {
-    case 'GET':
-      get(url, config, callback);
-      break;
-    case 'POST':
-      post(url, config, callback);
-      break;
-    case 'PUT':
-      put(url, config, callback);
-      break;
-    case 'DELETE':
-      destroy(url, config, callback);
-      break;
-  }
-
+    switch (config.method) {
+        case 'GET':
+            get(url, config, callback);
+            break;
+        case 'POST':
+            post(url, config, callback);
+            break;
+        case 'PUT':
+            put(url, config, callback);
+            break;
+        case 'DELETE':
+            destroy(url, config, callback);
+            break;
+    }
 }
 
-const end = (callback) => {
-  return (err, res) => {
-    const response = new Response();
-
-    if (err) {
-      response.setError(err);
-    } else {
-      response.setBody(res.body);
-      response.setHeaders(res.header);
-      response.setStatus(res.status);
+const passHeaders = (headers, req) => {
+    for (const header in headers) {
+        req = req.set(header, headers[header]);
     }
+    return req;
+};
 
-    callback(response);
-  }
+const end = (callback) => {
+    return (err, res) => {
+        const response = new Response();
+
+        if (err) {
+            response.setError(err);
+        } else {
+            response.setBody(res.body);
+            response.setHeaders(res.header);
+            response.setStatus(res.status);
+        }
+
+        callback(response);
+    }
 };
