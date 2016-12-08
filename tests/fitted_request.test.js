217ca543c265843c7a81a96caddc0ba9ab2b7a98
Joris Blaak <joris@label305.com>
2016-12-08 21:50:41 +0100

    Added @request handler [closes #18] (#27)
    Allows for adding configuration options to each request

---
+++ tests/fitted_request.test.js
@@ -0,0 +1,95 @@
+import expect from "expect.js";
+import Response from "../src/data/response";
+import {get, request} from "../src/fitted";
+
+describe('@request Decorator', function () {
+
+    it('Should have its method called with passed config', async function () {
+        /* Given */
+        let passedConfig = null;
+        const myRequestDecorator = (config) => {
+            passedConfig = config;
+
+            return config;
+        };
+
+        @request(myRequestDecorator)
+        class MyBackendService {
+
+            @get('https://your-backend.com/topstories.json')
+            topstories(request, response) {
+                return request(
+                    {
+                        driver: driver,
+                        foo: 'bar'
+                    },
+                    response
+                );
+            }
+
+        }
+
+        /* When */
+        var myBackendService = new MyBackendService();
+        await myBackendService.topstories();
+
+        /* Then */
+        expect(passedConfig.foo).to.be('bar');
+    });
+
+    it('should pass the resultant request config to the driver', async function () {
+        /* Given */
+        let passedConfig = null;
+        const myRequestDecorator = (config) => {
+            config.foo = 'boo';
+            
+            return config;
+        };
+
+        const myDriver = (url, config, callback) => {
+            passedConfig = config;
+            
+            const res = new Response();
+            res.setStatus(200);
+            res.setBody([
+                123,
+                321
+            ]);
+            callback(res);
+        };
+
+        @request(myRequestDecorator)
+        class MyBackendService {
+
+            @get('https://your-backend.com/topstories.json')
+            topstories(request, response) {
+                return request(
+                    {
+                        driver: myDriver,
+                        foo: 'bar'
+                    },
+                    response
+                );
+            }
+
+        }
+
+        /* When */
+        var myBackendService = new MyBackendService();
+        await myBackendService.topstories();
+
+        /* Then */
+        expect(passedConfig.foo).to.be('boo');
+    });
+
+});
+
+const driver = (url, config, callback) => {
+    const res = new Response();
+    res.setStatus(200);
+    res.setBody([
+        123,
+        321
+    ]);
+    callback(res);
+};
